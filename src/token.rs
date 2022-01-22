4f7881db70ed7f7ff7b4581bf98a7c6d57a568cf
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-22 22:24:48 +0900

    Tokenのテスト

--- src/token.rs
+++ src/token.rs
@@ -43,3 +43,28 @@ impl Token {
 pub fn tokenize(source: &str) -> Vec<Token> {
     source.chars().map(Token::from_char).collect()
 }
+
+#[cfg(test)]
+mod test {
+    use super::Token;
+
+    #[test]
+    fn test_token_from_char() {
+        fn helper(c: char, assert_token: Token) {
+            let token = Token::from_char(c);
+            assert_eq!(token, assert_token);
+        }
+
+        helper('>', Token::PtrIncrement);
+        helper('<', Token::PtrDecrement);
+        helper('+', Token::Increment);
+        helper('-', Token::Decrement);
+        helper('.', Token::Output);
+        helper(',', Token::Input);
+        helper('[', Token::WhileBegin);
+        helper(']', Token::WhileEnd);
+
+        helper('a', Token::Other('a'));
+        helper('1', Token::Other('1'));
+    }
+}
