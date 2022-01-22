a10b6effe02f9fbd8f3621cb02c6d78c58cb59e5
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-22 21:19:13 +0900

    token

---
+++ src/token.rs
@@ -0,0 +1,41 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Token {
+    PtrIncrement,
+    PtrDecrement,
+    Increment,
+    Decrement,
+    Output,
+    Input,
+    WhileBegin,
+    WhileEnd,
+    Other(char),
+}
+impl Token {
+    pub fn from_char(c: char) -> Token {
+        use Token::*;
+        match c {
+            '>' => PtrIncrement,
+            '<' => PtrDecrement,
+            '+' => Increment,
+            '-' => Decrement,
+            '.' => Output,
+            ',' => Input,
+            '[' => WhileBegin,
+            ']' => WhileEnd,
+            c => Other(c),
+        }
+    }
+    pub fn as_char(&self) -> char {
+        match self {
+            Token::PtrIncrement => '>',
+            Token::PtrDecrement => '<',
+            Token::Increment => '+',
+            Token::Decrement => '-',
+            Token::Output => '.',
+            Token::Input => ',',
+            Token::WhileBegin => '[',
+            Token::WhileEnd => ']',
+            Token::Other(c) => *c,
+        }
+    }
+}
