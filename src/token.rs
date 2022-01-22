8548b4f1aa2050c1da611730406b20c1ee1ef0e0
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-22 22:53:13 +0900

    MiddleToken

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::mem::swap;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Token {
     PtrIncrement,
@@ -42,6 +44,67 @@ impl Token {
 
 pub fn tokenize(source: &str) -> Vec<Token> {
     source.chars().map(Token::from_char).collect()
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum MiddleToken {
+    Token(Token, usize),
+    WhileBegin,
+    WhileEnd,
+}
+
+pub fn middle_token(tokens: &[Token]) -> Vec<MiddleToken> {
+    let mut middle_tokens = Vec::new();
+
+    let mut prev = None;
+    let mut prev_count = 0;
+
+    for token in tokens.iter().copied() {
+        match token {
+            Token::WhileBegin => {
+                if prev_count != 0 {
+                    middle_tokens.push(MiddleToken::Token(prev.unwrap(), prev_count));
+                }
+                middle_tokens.push(MiddleToken::WhileBegin);
+                prev = None;
+                prev_count = 0;
+                continue;
+            }
+            Token::WhileEnd => {
+                if prev_count != 0 {
+                    middle_tokens.push(MiddleToken::Token(prev.unwrap(), prev_count));
+                }
+                middle_tokens.push(MiddleToken::WhileEnd);
+                prev = None;
+                prev_count = 0;
+                continue;
+            }
+            _ => (),
+        }
+        if prev == None {
+            prev = Some(token);
+            prev_count = 1;
+        } else if prev == Some(token) {
+            prev_count += 1;
+        } else {
+            let mut token = Some(token);
+            swap(&mut prev, &mut token);
+
+            match token.unwrap() {
+                Token::WhileBegin | Token::WhileEnd | Token::Other(_) => (),
+                token => middle_tokens.push(MiddleToken::Token(token, prev_count)),
+            }
+            prev_count = 1;
+        }
+    }
+    if let Some(token) = prev {
+        match token {
+            Token::WhileBegin | Token::WhileEnd | Token::Other(_) => (),
+            token => middle_tokens.push(MiddleToken::Token(token, prev_count)),
+        }
+    }
+
+    middle_tokens
 }
 
 #[cfg(test)]
