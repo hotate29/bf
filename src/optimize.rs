a6be1720cdeb259efec535a2c7c3de92c093da38
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-24 23:38:07 +0900

    if_chain

--- src/optimize.rs
+++ src/optimize.rs
@@ -1,3 +1,5 @@
+use if_chain::if_chain;
+
 use crate::token::{ExprKind, Instruction, Node};
 
 pub fn optimize(mut root_node: Node) -> Node {
@@ -21,15 +23,17 @@ pub fn optimize(mut root_node: Node) -> Node {
 
 /// [-]をSetValue(0)に変換する
 fn opt_zeroset(expr: &ExprKind) -> Option<ExprKind> {
-    if let ExprKind::While(while_node) = expr {
-        if let [ExprKind::Instructions(instructions)] = while_node.0.as_slice() {
-            if let [Instruction::Decrement(1)] = instructions.as_slice() {
-                return Some(ExprKind::Instructions(vec![Instruction::SetValue(0)]));
-            }
+    if_chain! {
+        if let ExprKind::While(while_node) = expr;
+        if let [ExprKind::Instructions(instructions)] = while_node.0.as_slice();
+        if let [Instruction::Decrement(1)] = instructions.as_slice();
+        then {
+            Some(ExprKind::Instructions(vec![Instruction::SetValue(0)]))
+        }
+        else {
+            None
         }
     }
-
-    None
 }
 
 #[cfg(test)]
