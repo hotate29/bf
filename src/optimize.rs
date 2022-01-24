7894ca66baa2616af9c552f8ffa6305999c32c36
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-24 22:19:55 +0900

    ゼロ代入の最適化 [-]をSetValue(0)に置換する

--- src/optimize.rs
+++ src/optimize.rs
@@ -1,7 +1,10 @@
-use crate::token::{ExprKind, Node};
+use crate::token::{ExprKind, Instruction, Node};
 
 pub fn optimize(mut root_node: Node) -> Node {
     fn inner(expr: &mut ExprKind) {
+        if let Some(optimized_node) = opt_zeroset(expr) {
+            *expr = optimized_node;
+        }
         if let ExprKind::While(while_node) = expr {
             for expr in &mut while_node.0 {
                 inner(expr)
@@ -14,4 +17,17 @@ pub fn optimize(mut root_node: Node) -> Node {
     }
 
     root_node
+}
+
+/// [-]をSetValue(0)に変換する
+fn opt_zeroset(expr: &ExprKind) -> Option<ExprKind> {
+    if let ExprKind::While(while_node) = expr {
+        if let [ExprKind::Instructions(instructions)] = while_node.0.as_slice() {
+            if let [Instruction::Decrement(1)] = instructions.as_slice() {
+                return Some(ExprKind::Instructions(vec![Instruction::SetValue(0)]));
+            }
+        }
+    }
+
+    None
 }
