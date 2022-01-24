05365c9b9afacb39d14bb991f049ba52ccb5d11c
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-24 21:57:56 +0900

    入力と出力を受け取るようにした

--- src/interprinter.rs
+++ src/interprinter.rs
@@ -1,12 +1,14 @@
 use crate::token::{ExprKind, Instruction, Node};
 
-use std::io::{prelude::*, stdin, stdout};
+use std::io::prelude::*;
 
-struct State {
+struct State<R: Read, W: Write> {
     pointer: usize,
     memory: Vec<u8>,
+    input_reader: R,
+    output_writer: W,
 }
-impl State {
+impl<R: Read, W: Write> State<R, W> {
     fn add(&mut self, value: u8) {
         self.memory[self.pointer] = self.memory[self.pointer].wrapping_add(value);
     }
@@ -22,35 +24,39 @@ impl State {
     fn set_value(&mut self, value: u8) {
         self.memory[self.pointer] = value
     }
-    fn output(&self) {
-        print!(
+    fn output(&mut self) {
+        write!(
+            self.output_writer,
             "{}",
             char::from_u32(self.memory[self.pointer] as u32).unwrap()
-        );
-        stdout().flush().unwrap();
+        )
+        .unwrap();
+        self.output_writer.flush().unwrap();
     }
     fn input(&mut self) {
         let mut buf = [0];
-        stdin().read_exact(&mut buf).unwrap();
+        self.input_reader.read_exact(&mut buf).unwrap();
         self.set_value(buf[0]);
     }
 }
 
-pub struct InterPrinter {
-    state: State,
+pub struct InterPrinter<R: Read, W: Write> {
+    state: State<R, W>,
     root_node: Node,
 }
-impl InterPrinter {
-    pub fn new(root_node: Node, memory_len: usize) -> Self {
+impl<R: Read, W: Write> InterPrinter<R, W> {
+    pub fn new(root_node: Node, memory_len: usize, input: R, output: W) -> Self {
         let state = State {
             pointer: 0,
             memory: vec![0; memory_len],
+            input_reader: input,
+            output_writer: output,
         };
 
         Self { state, root_node }
     }
     pub fn start(&mut self) {
-        fn inner(state: &mut State, node: &Node) {
+        fn inner<R: Read, W: Write>(state: &mut State<R, W>, node: &Node) {
             for expr in &node.0 {
                 match expr {
                     ExprKind::Instructions(instructions) => {
