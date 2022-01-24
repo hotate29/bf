498f8f74c8c1561da3687844056bb7d43cb056ec
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-25 00:22:23 +0900

    opt_set_value

--- src/optimize.rs
+++ src/optimize.rs
@@ -4,6 +4,9 @@ use crate::token::{ExprKind, Instruction, Node};
 
 pub fn optimize(mut root_node: Node) -> Node {
     fn inner(node: &mut Node) {
+        if let Some(optimized_node) = opt_set_value(node) {
+            *node = optimized_node;
+        }
         for expr in &mut node.0 {
             // ExprKindを最適化する
             if let Some(optimized_expr) = opt_zeroset(expr) {
@@ -34,6 +37,39 @@ fn opt_zeroset(expr: &ExprKind) -> Option<ExprKind> {
             None
         }
     }
+}
+
+fn opt_set_value(node: &Node) -> Option<Node> {
+    for i in 0..node.0.len() {
+        let front_kinds = &node.0[0..i];
+
+        if_chain! {
+            if let &[ExprKind::Instructions(instructions), ExprKind::While(while_node), ExprKind::Instructions(s), last_kinds @ ..] =
+            &node.0.as_slice();
+            if let [front_instructions @ .., Instruction::Increment(n)] = instructions.as_slice();
+            if let [ExprKind::Instructions(while_instructions)] = while_node.0.as_slice();
+            if let [Instruction::Decrement(1), Instruction::PtrIncrement(ptrinc_count), Instruction::Increment(x), Instruction::PtrDecrement(ptrdec_count)] = while_instructions.as_slice();
+            if ptrinc_count == ptrdec_count;
+            then {
+                eprintln!("a {} {} {}", n * x, n, x);
+                let x = n * x;
+                let x = (x % u8::MAX as usize) as u8;
+
+                let mut node_kinds = front_kinds.to_vec();
+
+                let mut instructions = front_instructions.to_vec();
+                instructions.push(Instruction::SetToValue(*ptrinc_count, x));
+                instructions.extend_from_slice(s);
+                node_kinds.push(ExprKind::Instructions(instructions));
+
+                node_kinds.extend_from_slice(last_kinds);
+
+                let optimized_node = Node(node_kinds);
+                return Some(optimized_node);
+            }
+        }
+    }
+    None
 }
 
 #[cfg(test)]
