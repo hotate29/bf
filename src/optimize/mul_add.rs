b1dc56194b59f9f360ea2b7c3b91b4d24aab362e
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-28 23:48:17 +0900

    if_chain

--- src/optimize/mul_add.rs
+++ src/optimize/mul_add.rs
@@ -10,24 +10,25 @@ pub struct MulAddOptimizer;
 impl Optimizer for MulAddOptimizer {
     /// [->>>+++<<<]を変換する
     fn optimize_expr(&self, expr: &ExprKind) -> Option<ExprKind> {
-        if let ExprKind::While(while_node) = expr {
-            if let [ExprKind::Instructions(while_instructions)] = while_node.0.as_slice() {
-                if let [Instruction::PtrIncrement(ptr_increment), Instruction::Add(add_count), Instruction::PtrDecrement(ptr_decrement), Instruction::Sub(1)]
-                | [Instruction::Sub(1), Instruction::PtrIncrement(ptr_increment), Instruction::Add(add_count), Instruction::PtrDecrement(ptr_decrement)] =
-                    while_instructions.as_slice()
-                {
-                    if ptr_increment == ptr_decrement {
-                        info!("optimize!");
-                        let add_count = (add_count % u8::MAX as usize) as u8;
-                        let expr = ExprKind::Instructions(vec![Instruction::MulAdd(
-                            *ptr_increment,
-                            add_count,
-                        )]);
-                        return Some(expr);
-                    }
-                }
+        if_chain! {
+            if let ExprKind::While(while_node) = expr;
+            if let [ExprKind::Instructions(while_instructions)] = while_node.0.as_slice();
+            if let [Instruction::PtrIncrement(ptr_increment), Instruction::Add(add_count), Instruction::PtrDecrement(ptr_decrement), Instruction::Sub(1)]
+                |  [Instruction::Sub(1), Instruction::PtrIncrement(ptr_increment), Instruction::Add(add_count), Instruction::PtrDecrement(ptr_decrement)]
+                = while_instructions.as_slice();
+            if ptr_increment == ptr_decrement;
+            then {
+                info!("optimize!");
+                let add_count = (add_count % u8::MAX as usize) as u8;
+                let expr = ExprKind::Instructions(vec![Instruction::MulAdd(
+                    *ptr_increment,
+                    add_count,
+                )]);
+                Some(expr)
+            }
+            else {
+                None
             }
         }
-        None
     }
 }
