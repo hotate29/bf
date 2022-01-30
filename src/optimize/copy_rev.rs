428891caab1d4383053cba707b1cb36d5f031c41
hotate29 <hotate_oc@yahoo.co.jp>
2022-01-31 00:24:28 +0900

    コピー最適化のパターンを増やした

--- src/optimize/copy_rev.rs
+++ src/optimize/copy_rev.rs
@@ -8,6 +8,8 @@ use super::{ExprKind, Optimizer};
 pub struct CopyRevOptimizer;
 
 impl Optimizer for CopyRevOptimizer {
+    // [-<<+>+>]
+    // [->+>+<<]
     fn optimize_expr(&self, expr: &super::ExprKind) -> Option<super::ExprKind> {
         if_chain! {
             if let ExprKind::While(while_node) = expr;
@@ -27,6 +29,26 @@ impl Optimizer for CopyRevOptimizer {
             else {
                 None
             }
-        }
+        }.or_else(||{
+            if_chain! {
+                if let ExprKind::While(while_node) = expr;
+                if let [ExprKind::Instructions(while_instructions)] = while_node.0.as_slice();
+                if let [Instruction::Sub(1), Instruction::PtrDecrement(x), Instruction::Add(1), Instruction::PtrIncrement(y), Instruction::Add(1), Instruction::PtrIncrement(z)] =
+                    while_instructions.as_slice();
+                if *x == y + z;
+                then {
+                    info!("optimize! 2");
+                    let expr = ExprKind::Instructions(vec![
+                        Instruction::CopyRev(*x),
+                        Instruction::CopyRev(x - y),
+                        Instruction::ZeroSet,
+                    ]);
+                    Some(expr)
+                }
+                else {
+                    None
+                }
+            }
+        })
     }
 }
