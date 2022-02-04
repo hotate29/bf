938094c7f92e5bd085ba1cb094319a4efa20653f
hotate29 <hotate_oc@yahoo.co.jp>
2022-02-05 01:24:24 +0900

    微修正

--- src/bin/interprinter.rs
+++ src/bin/interprinter.rs
@@ -11,19 +11,19 @@ fn main() {
 
     let code = fs::read_to_string(&args[1]).unwrap();
 
-    let optimize_flag = args.get(2) == Some(&"O".to_string());
+    let optimize_flag = args.get(2).map_or(false, |arg| arg == "O");
 
     let mut root_node = Node::from_source(&code).unwrap();
     if optimize_flag {
         root_node = optimize(root_node, &all_optimizer());
     }
 
-    let step = InterPrinter::builder()
+    let step_count = InterPrinter::builder()
         .root_node(&root_node)
         .input(io::stdin())
         .output(io::stdout())
         .build()
         .count();
 
-    eprintln!("step: {step}");
+    eprintln!("step: {step_count}");
 }
