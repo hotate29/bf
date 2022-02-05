93812fb6afedf1bffde77c2b1d1ebc27ca602e90
hotate29 <hotate_oc@yahoo.co.jp>
2022-02-06 01:44:16 +0900

    わかりやすく

--- src/transcompile.rs
+++ src/transcompile.rs
@@ -10,7 +10,7 @@ pub fn to_c(root_node: &Node) -> String {
                         match instruction {
                             Instruction::PtrIncrement(n) => c_code.push_str(&format!("ptr+={n};")),
                             Instruction::PtrDecrement(n) => c_code.push_str(&format!("ptr-={n};")),
-                            Instruction::Add(n) => c_code.push_str(&format!("ptr[0]+={n};")),
+                            Instruction::Add(n) => c_code.push_str(&format!("*ptr+={n};")),
                             Instruction::AddTo(offset) | Instruction::Copy(offset) => {
                                 c_code.push_str(&format!("ptr[{offset}]+=ptr[0];"))
                             }
@@ -20,7 +20,7 @@ pub fn to_c(root_node: &Node) -> String {
                             Instruction::SubToRev(n) => {
                                 c_code.push_str(&format!("if(ptr[0]!=0){{*(ptr-{n})-=ptr[0];}}"))
                             }
-                            Instruction::Sub(n) => c_code.push_str(&format!("ptr[0]-={n};")),
+                            Instruction::Sub(n) => c_code.push_str(&format!("*ptr-={n};")),
                             Instruction::Output(n) => {
                                 for _ in 0..*n {
                                     c_code.push_str("putchar(ptr[0]);")
@@ -40,7 +40,7 @@ pub fn to_c(root_node: &Node) -> String {
                                 ));
                             }
                             Instruction::ZeroSet => {
-                                c_code.push_str("ptr[0]=0;");
+                                c_code.push_str("*ptr=0;");
                             }
                         }
                     }
@@ -57,7 +57,7 @@ pub fn to_c(root_node: &Node) -> String {
     let mut a = String::new();
     inner(root_node, &mut a);
 
-    let mut c_code = String::from("#include <stdio.h>\n#include <stdint.h>\n\nint main(void){\nuint8_t mem[30000] = {0};\nuint8_t* ptr = mem;");
+    let mut c_code = String::from("#include <stdio.h>\n#include <stdint.h>\nint main(void){uint8_t mem[30000]={0};uint8_t* ptr = mem;");
     c_code += &a;
     c_code += "}";
 
