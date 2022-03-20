50cca2247a72f51da7bf4fd03382bc6e21c874cc
hotate29 <hotate_oc@yahoo.co.jp>
2022-03-20 20:09:06 +0900

    Node, ExprKindを移動

--- src/parse.rs
+++ src/parse.rs
@@ -2,6 +2,8 @@ use std::fmt::Display;
 
 use serde::Serialize;
 
+use crate::token::{middle_token, Instruction, MiddleToken, ParseError};
+
 #[derive(Debug, Clone, Copy, PartialEq, Serialize)]
 pub enum Token {
     /// >
@@ -57,6 +59,145 @@ impl Display for Token {
 
 pub fn tokenize(code: &str) -> Vec<Token> {
     code.chars().filter_map(Token::from_char).collect()
+}
+
+// [++[>>]-][]+
+// root Node
+//   |-while
+//   | |-(+2)
+//   | |-while
+//   | |  |-(>2)
+//   | |-(-1)
+//   |
+//   |-while
+//   |-(+1)
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize)]
+pub enum ExprKind {
+    Instructions(Vec<Instruction>),
+    While(Node),
+}
+
+impl ExprKind {
+    pub fn concat(&self, other: &ExprKind) -> Option<ExprKind> {
+        if let (
+            ExprKind::Instructions(self_instructions),
+            ExprKind::Instructions(other_instructions),
+        ) = (self, other)
+        {
+            let mut self_instructions = self_instructions.clone();
+            self_instructions.extend(other_instructions);
+
+            Some(ExprKind::Instructions(self_instructions))
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize)]
+pub struct Node(pub Vec<ExprKind>);
+
+impl Node {
+    pub fn from_source(source: &str) -> Result<Node, ParseError> {
+        let tokens = tokenize(source);
+        let middle_token = middle_token(&tokens)?;
+        Ok(Node::from_middle_tokens(&middle_token))
+    }
+    pub fn from_middle_tokens(tokens: &[MiddleToken]) -> Node {
+        fn inner(tokens: &[MiddleToken]) -> (usize, Node) // (どれだけ進んだか, Node)
+        {
+            let mut exprs = Vec::new();
+            let mut index = 0;
+            let mut last_while_end_index = None;
+
+            while index < tokens.len() {
+                let token = tokens[index];
+
+                match token {
+                    MiddleToken::Token(_, _) => index += 1,
+                    MiddleToken::WhileBegin => {
+                        {
+                            let sub_tokens = &tokens[last_while_end_index.unwrap_or(0)..index];
+                            if !sub_tokens.is_empty() {
+                                exprs.push(ExprKind::Instructions(
+                                    sub_tokens
+                                        .iter()
+                                        .map(|token| token.to_instruction().unwrap())
+                                        .collect(),
+                                ));
+                            }
+                        }
+                        {
+                            index += 1;
+                            let (count, while_node) = inner(&tokens[index..]);
+                            index += count;
+                            last_while_end_index = Some(index);
+                            exprs.push(ExprKind::While(while_node));
+                        }
+                    }
+                    MiddleToken::WhileEnd => {
+                        {
+                            let sub_tokens = &tokens[last_while_end_index.unwrap_or(0)..index];
+                            if !sub_tokens.is_empty() {
+                                let expr = ExprKind::Instructions(
+                                    sub_tokens
+                                        .iter()
+                                        .map(|token| token.to_instruction().unwrap())
+                                        .collect(),
+                                );
+                                exprs.push(expr)
+                            }
+                        }
+
+                        let node = Node(exprs);
+                        return (index + 1, node);
+                    }
+                }
+            }
+
+            let range = last_while_end_index.unwrap_or(0)..index;
+            if !range.is_empty() {
+                exprs.push(ExprKind::Instructions(
+                    tokens[range]
+                        .iter()
+                        .map(|token| token.to_instruction().unwrap())
+                        .collect(),
+                ))
+            }
+            (index, Node(exprs))
+        }
+        let (c, node) = inner(tokens);
+        assert_eq!(c, tokens.len());
+        node
+    }
+}
+
+impl ToString for Node {
+    fn to_string(&self) -> String {
+        fn inner(node: &Node, out: &mut String) {
+            for expr in &node.0 {
+                match expr {
+                    ExprKind::Instructions(instructions) => {
+                        for instruction in instructions {
+                            if let Some(s) = instruction.to_compressed_string() {
+                                out.push_str(&s);
+                            } else {
+                                out.push_str("None");
+                            }
+                        }
+                    }
+                    ExprKind::While(while_node) => {
+                        out.push('[');
+                        inner(while_node, out);
+                        out.push(']');
+                    }
+                }
+            }
+        }
+        let mut out = String::new();
+        inner(self, &mut out);
+        out
+    }
 }
 
 #[cfg(test)]
