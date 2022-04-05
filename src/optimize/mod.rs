c3536e6bb9f1d0a7d1bdb4549db7f3082d7e3948
hotate29 <hotate_oc@yahoo.co.jp>
2022-04-06 01:43:51 +0900

    古いOptimizerたちを削除

--- src/optimize/mod.rs
+++ src/optimize/mod.rs
@@ -5,96 +5,6 @@ use crate::{
     parse::{Node, Nodes},
 };
 
-pub struct AddOptimizer;
-
-impl Optimizer for AddOptimizer {
-    fn optimize_node(&self, node: &Node) -> Option<Nodes> {
-        if let Node::Loop(loop_nodes) = node {
-            if loop_nodes.len() == 4 {
-                let mut nodes_iter = loop_nodes.iter();
-
-                if let [Node::Instruction(Sub(1)), Node::Instruction(PtrIncrement(ptr_increment)), Node::Instruction(Add(1)), Node::Instruction(PtrDecrement(ptr_decrement))]
-                | [Node::Instruction(PtrIncrement(ptr_increment)), Node::Instruction(Add(1)), Node::Instruction(PtrDecrement(ptr_decrement)), Node::Instruction(Sub(1))] = {
-                    [
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                    ]
-                } {
-                    if ptr_increment == ptr_decrement {
-                        return Some(
-                            [AddTo(*ptr_increment as isize).into(), ZeroSet.into()].into(),
-                        );
-                    }
-                }
-
-                let mut nodes_iter = loop_nodes.iter();
-
-                if let [Node::Instruction(Sub(1)), Node::Instruction(PtrDecrement(ptr_increment)), Node::Instruction(Add(1)), Node::Instruction(PtrIncrement(ptr_decrement))]
-                | [Node::Instruction(PtrDecrement(ptr_increment)), Node::Instruction(Add(1)), Node::Instruction(PtrIncrement(ptr_decrement)), Node::Instruction(Sub(1))] = [
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                ] {
-                    if ptr_decrement == ptr_increment {
-                        return Some(
-                            [AddTo(-(*ptr_decrement as isize)).into(), ZeroSet.into()].into(),
-                        );
-                    }
-                }
-            }
-        }
-        None
-    }
-}
-
-pub struct SubOptimizer;
-
-impl Optimizer for SubOptimizer {
-    fn optimize_node(&self, node: &Node) -> Option<Nodes> {
-        if let Node::Loop(loop_nodes) = node {
-            if loop_nodes.len() == 4 {
-                let mut nodes_iter = loop_nodes.iter();
-
-                if let [Node::Instruction(Sub(1)), Node::Instruction(PtrIncrement(ptr_increment)), Node::Instruction(Sub(1)), Node::Instruction(PtrDecrement(ptr_decrement))]
-                | [Node::Instruction(PtrIncrement(ptr_increment)), Node::Instruction(Sub(1)), Node::Instruction(PtrDecrement(ptr_decrement)), Node::Instruction(Sub(1))] = {
-                    [
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                        nodes_iter.next()?,
-                    ]
-                } {
-                    if ptr_increment == ptr_decrement {
-                        return Some(
-                            [SubTo(*ptr_increment as isize).into(), ZeroSet.into()].into(),
-                        );
-                    }
-                }
-
-                let mut nodes_iter = loop_nodes.iter();
-
-                if let [Node::Instruction(Sub(1)), Node::Instruction(PtrDecrement(ptr_increment)), Node::Instruction(Sub(1)), Node::Instruction(PtrIncrement(ptr_decrement))]
-                | [Node::Instruction(PtrDecrement(ptr_increment)), Node::Instruction(Sub(1)), Node::Instruction(PtrIncrement(ptr_decrement)), Node::Instruction(Sub(1))] = [
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                    nodes_iter.next()?,
-                ] {
-                    if ptr_decrement == ptr_increment {
-                        return Some(
-                            [SubTo(-(*ptr_decrement as isize)).into(), ZeroSet.into()].into(),
-                        );
-                    }
-                }
-            }
-        }
-        None
-    }
-}
-
 fn merge_instruction(nodes: Nodes) -> Nodes {
     let mut new_nodes = Nodes::new();
 
@@ -265,7 +175,7 @@ pub fn offset_opt(nodes: &Nodes) -> Nodes {
             && !has_output
             && state.offset_map
                 .get(&0)
-                .filter(|ins| ins.inner() == &[Sub(1)])
+                .filter(|ins| ins.inner() == &[Sub(1)] || ins.inner() == &[Add(1)])
                 .is_some()
         {
             // 最適化をするぞ！バリバリ！
@@ -274,14 +184,15 @@ pub fn offset_opt(nodes: &Nodes) -> Nodes {
             for (offset, instructions) in state.offset_map {
                 for instruction in instructions.inner {
                     let instruction = match instruction {
+                        // 最後にZeroSetにする
+                        Sub(1) | Add(1) if offset == 0 => continue,
                         Add(1) => AddTo(offset),
                         Add(value) => MulAdd(offset, value),
-                        Sub(1) if offset == 0 => continue,
                         Sub(1) => SubTo(offset),
                         Output(repeat) => OutputOffset(repeat, offset),
                         // Input(_) => todo!(),
                         // ZeroSet => ZeroSetOffset(offset),
-                        _ => panic!(),
+                        ins => panic!("{ins:?}"),
                     };
                     new_nodes.push_back(instruction.into());
                 }
@@ -300,41 +211,13 @@ pub fn offset_opt(nodes: &Nodes) -> Nodes {
     }
 }
 
-pub trait Optimizer {
-    fn optimize_node(&self, node: &Node) -> Option<Nodes>;
-}
-
-pub struct ZeroSetOptimizer;
-
-impl Optimizer for ZeroSetOptimizer {
-    fn optimize_node(&self, node: &Node) -> Option<Nodes> {
-        if let Node::Loop(loop_nodes) = node {
-            if loop_nodes.len() == 1 {
-                if let Node::Instruction(Add(1) | Sub(1)) = loop_nodes.front()? {
-                    let nodes = [ZeroSet.into()].into();
-                    return Some(nodes);
-                }
-                if let Node::Instruction(AddOffset(offset, 1) | SubOffset(offset, 1)) =
-                    loop_nodes.front()?
-                {
-                    let nodes = [ZeroSetOffset(*offset).into()].into();
-                    return Some(nodes);
-                }
-            }
-        }
-        None
-    }
-}
-
 pub fn optimize(nodes: &Nodes) -> Nodes {
     offset_opt(nodes)
 }
 
 #[cfg(test)]
 mod test {
-    use super::{
-        merge_instruction, offset_opt, AddOptimizer, Optimizer, SubOptimizer, ZeroSetOptimizer,
-    };
+    use super::{merge_instruction, offset_opt};
     use crate::{
         instruction::Instruction::*,
         parse::{tokenize, Node, Nodes},
@@ -354,60 +237,21 @@ mod test {
         .into();
         assert_eq!(merge_instruction(nodes), [Add(1).into()].into());
     }
-    fn optimize_node(code: &str, optimizer: impl Optimizer) -> Option<Nodes> {
-        let tokens = tokenize(code);
-        let mut nodes = Node::from_tokens(tokens).unwrap();
-        if nodes.len() == 1 {
-            if let Node::Loop(loop_nodes) = nodes.pop_front().unwrap() {
-                let merged_loop_node = merge_instruction(loop_nodes);
-                let loop_node = Node::Loop(merged_loop_node);
-
-                optimizer.optimize_node(&loop_node)
-            } else {
-                panic!()
-            }
-        } else {
-            panic!()
-        }
-    }
-
-    fn assert_node(optimizer: impl Optimizer, code: &str, node: Option<Nodes>) {
-        let optimized_node = optimize_node(code, optimizer);
-        assert_eq!(node, optimized_node);
-    }
 
     #[rstest(input, expected,
-        case("[-]", Some([ZeroSet.into()].into())),
-        case("[+]", Some([ZeroSet.into()].into())),
-        case("[++]", None),
-    )]
-    fn test_zeroset_opt(input: &str, expected: Option<Nodes>) {
-        assert_node(ZeroSetOptimizer, input, expected);
-    }
-
-    #[rstest(input, expected,
-        case("[->>>+<<<]", Some([AddTo(3).into(), ZeroSet.into()].into())),
-        case("[>>>+<<<-]", Some([AddTo(3).into(), ZeroSet.into()].into())),
-        case("[-<<<+>>>]", Some([AddTo(-3).into(), ZeroSet.into()].into())),
-        case("[<<<+>>>-]", Some([AddTo(-3).into(), ZeroSet.into()].into())),
-        case("[-<<<++>>>]", None),
-    )]
-    fn test_add_opt(input: &str, expected: Option<Nodes>) {
-        assert_node(AddOptimizer, input, expected);
-    }
-
-    #[rstest(input, expected,
-        case("[->>>-<<<]", Some([SubTo(3).into(), ZeroSet.into()].into())),
-        case("[>>>-<<<-]", Some([SubTo(3).into(), ZeroSet.into()].into())),
-        case("[-<<<->>>]", Some([SubTo(-3).into(), ZeroSet.into()].into())),
-        case("[<<<->>>-]", Some([SubTo(-3).into(), ZeroSet.into()].into())),
-        case("[-<<<-->>>]", None),
-    )]
-    fn test_sub_opt(input: &str, expected: Option<Nodes>) {
-        assert_node(SubOptimizer, input, expected);
-    }
-
-    #[rstest(input, expected,
+        case("[-]", [ZeroSet.into()].into()),
+        case("[+]", [ZeroSet.into()].into()),
+        case("[++]", [Node::Loop([AddOffset(0, 2).into()].into())].into()),
+        case("[->>>+<<<]", [AddTo(3).into(), ZeroSet.into()].into()),
+        case("[>>>+<<<-]", [AddTo(3).into(), ZeroSet.into()].into()),
+        case("[-<<<+>>>]", [AddTo(-3).into(), ZeroSet.into()].into()),
+        case("[<<<+>>>-]", [AddTo(-3).into(), ZeroSet.into()].into()),
+        case("[-<<<++>>>]", [MulAdd(-3, 2).into(), ZeroSet.into()].into()),
+        case("[->>>-<<<]", [SubTo(3).into(), ZeroSet.into()].into()),
+        case("[>>>-<<<-]", [SubTo(3).into(), ZeroSet.into()].into()),
+        case("[-<<<->>>]", [SubTo(-3).into(), ZeroSet.into()].into()),
+        case("[<<<->>>-]", [SubTo(-3).into(), ZeroSet.into()].into()),
+        // case("[-<<<-->>>]", [SubOffset(0, 1).into(), SubOffset(-3, 2).into()].into()),
         case("", [].into()),
         case("+++", [AddOffset(0, 3).into()].into()),
         case("+++---", [].into()),
@@ -420,13 +264,13 @@ mod test {
         case("[>>>-<<<-]", [SubTo(3).into(), ZeroSet.into()].into()),
         case("[>>>->+<<<<-]", [SubTo(3).into(), AddTo(4).into(), ZeroSet.into()].into()),
         case("+++[>>>[-][[->+<]]<<<]", [AddOffset(0, 3).into(), Node::Loop([PtrIncrement(3).into(), ZeroSet.into(), Node::Loop([AddTo(1).into(), ZeroSet.into()].into()), PtrDecrement(3).into()].into())].into()),
-        case("[->>>.<<<]", [Node::Loop([SubOffset(0, 1).into(), OutputOffset(1,3).into()].into())].into()),
+        case("[->>>.<<<]", [Node::Loop([OutputOffset(1,3).into(), SubOffset(0, 1).into()].into())].into()),
         case("[->+>+>++>+++<<<<]", [AddTo(1).into(), AddTo(2).into(), MulAdd(3, 2).into(), MulAdd(4, 3).into(), ZeroSet.into()].into()),
         // TODO: MulSubを実装する
         #[should_panic]
         case("[-<<<-->>>]", [Node::Loop([SubOffset(0, 1).into(), SubOffset(-3, 2).into()].into())].into()),
     )]
-    fn test_offset_opt(input: &str, expected: Nodes) {
+    fn test_optimize(input: &str, expected: Nodes) {
         let tokens = tokenize(input);
         let nodes = Node::from_tokens(tokens).unwrap();
 
