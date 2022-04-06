35a1146a82ff80be2942cf099b5b8364ff3453bf
hotate29 <hotate_oc@yahoo.co.jp>
2022-04-07 01:47:16 +0900

    帰ってきたトランスコンパイル

--- src/transcompile.rs
+++ src/transcompile.rs
@@ -15,17 +15,17 @@ pub fn to_c2(root_node: &Nodes) -> String {
                     Instruction::PtrDecrement(n) => c_code.push_str(&format!("ptr-={n};")),
                     Instruction::Add(n) => c_code.push_str(&format!("*ptr+={n};")),
                     Instruction::AddTo(to_offset, offset) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{to_offset}]+=ptr[{offset}];"))
+                        c_code.push_str(&format!("*(ptr+{to_offset})+=ptr[{offset}];"))
                     }
                     Instruction::AddTo(to_offset, offset) if *offset < 0 => c_code.push_str(
-                        &format!("if(ptr[0]!=0){{*(ptr{to_offset})+=ptr[{offset}];}}"),
+                        &format!("if(*(ptr+{offset})!=0){{*(ptr+{to_offset})+=*(ptr+{offset});}}"),
                     ),
                     Instruction::Sub(n) => c_code.push_str(&format!("*ptr-={n};")),
                     Instruction::SubTo(to_offset, offset) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{to_offset}]-=ptr[{offset}];"))
+                        c_code.push_str(&format!("*(ptr+{to_offset})-=ptr[{offset}];"))
                     }
                     Instruction::SubTo(to_offset, offset) if *offset < 0 => c_code.push_str(
-                        &format!("if(ptr[0]!=0){{*(ptr{to_offset})-=ptr[{offset}];}}"),
+                        &format!("if(*(ptr+{offset})!=0){{*(ptr+{to_offset})-=*(ptr+{offset});}}"),
                     ),
                     Instruction::Output(n) => {
                         for _ in 0..*n {
@@ -38,58 +38,43 @@ pub fn to_c2(root_node: &Nodes) -> String {
                         }
                     }
                     Instruction::MulAdd(to_offset, offset, value) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{offset}]+={value}*ptr[0];"));
+                        c_code.push_str(&format!("*(ptr+{to_offset})+={value}*ptr[{offset}];"));
                     }
                     Instruction::MulAdd(to_offset, offset, value) if *offset < 0 => {
                         c_code.push_str(&format!(
-                            "if(*ptr!=0){{*(ptr{to_offset})+={value}*ptr[{offset}];}}"
+                            "if(*(ptr+{offset})!=0){{*(ptr+{to_offset})+={value}**(ptr+{offset});}}"
                         ));
-                        unimplemented!()
                     }
                     Instruction::MulSub(to_offset, offset, value) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{offset}]-={value}*ptr[0];"));
-                        unimplemented!()
+                        c_code.push_str(&format!("*(ptr+{to_offset})-={value}*ptr[{offset}];"));
                     }
                     Instruction::MulSub(to_offset, offset, value) if *offset < 0 => {
-                        c_code.push_str(&format!("if(*ptr!=0){{*(ptr{offset})-={value}*ptr[0];}}"));
-                        unimplemented!()
+                        c_code.push_str(&format!(
+                            "if(*(ptr+{offset})!=0){{*(ptr+{to_offset})-={value}**(ptr+{offset});}}"
+                        ));
                     }
                     Instruction::ZeroSet => {
                         c_code.push_str("*ptr=0;");
                     }
-                    Instruction::AddOffset(offset, value) if *offset < 0 => {
-                        c_code.push_str(&format!("if(*ptr!=0){{*(ptr{offset})+={value};}}"))
+                    Instruction::AddOffset(offset, value) => {
+                        c_code.push_str(&format!("*(ptr+{offset})+={value};"))
                     }
-                    Instruction::AddOffset(offset, value) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{offset}]+={value};"))
+                    Instruction::SubOffset(offset, value) => {
+                        c_code.push_str(&format!("*(ptr+{offset})-={value};"))
                     }
-                    Instruction::SubOffset(offset, value) if *offset < 0 => {
-                        c_code.push_str(&format!("if(*ptr!=0){{*(ptr{offset})-={value};}}"))
-                    }
-                    Instruction::SubOffset(offset, value) if *offset >= 0 => {
-                        c_code.push_str(&format!("ptr[{offset}]-={value};"))
-                    }
-                    Instruction::OutputOffset(offset, repeat) if *offset < 0 => {
+                    Instruction::OutputOffset(offset, repeat) => {
                         for _ in 0..*repeat {
-                            c_code.push_str(&format!("if(*ptr!=0){{putchar(*(ptr{offset}));}}"))
+                            c_code.push_str(&format!("putchar(*(ptr+{offset}));"))
                         }
                     }
-                    Instruction::OutputOffset(offset, repeat) if *offset >= 0 => {
-                        for _ in 0..*repeat {
-                            c_code.push_str(&format!("putchar(ptr[{offset}]);"))
-                        }
-                    }
-                    Instruction::InputOffset(offset, repeat) if *offset >= 0 => {
-                        for _ in 0..*repeat {
-                            c_code.push_str(&format!("ptr[{offset}]=getchar();"))
-                        }
-                    }
-                    Instruction::InputOffset(offset, repeat) if *offset < 0 => {
+                    Instruction::InputOffset(offset, repeat) => {
                         for _ in 0..*repeat {
                             c_code.push_str(&format!("*(ptr+{offset})=getchar();"))
                         }
                     }
-                    Instruction::ZeroSetOffset(_) => todo!(),
+                    Instruction::ZeroSetOffset(offset) => {
+                        c_code.push_str(&format!("*(ptr+{offset})=0;"))
+                    }
                     ins => panic!("unimplemented instruction. {ins:?}"),
                     // Instruction::Copy(_) => todo!(),
                 },
