a64f2a4513d28f98d86356bde661f4bde8dc5165
hotate29 <hotate_oc@yahoo.co.jp>
2022-04-10 01:29:05 +0900

    時間計測

--- src/bin/interprinter.rs
+++ src/bin/interprinter.rs
@@ -1,10 +1,24 @@
-use std::{env::args, fs, io};
+use std::{
+    env::args,
+    fs, io,
+    time::{Duration, Instant},
+};
 
 use bf::{
     interprinter::InterPrinter,
     optimize::optimize,
     parse::{tokenize, Node},
 };
+use log::info;
+
+fn time_keisoku<F, T>(func: F) -> (T, Duration)
+where
+    F: FnOnce() -> T,
+{
+    let instant = Instant::now();
+    let func_out = func();
+    (func_out, instant.elapsed())
+}
 
 fn main() {
     env_logger::init();
@@ -18,15 +32,21 @@ fn main() {
     let mut root_node = Node::from_tokens(tokens).unwrap();
 
     if optimize_flag {
-        root_node = optimize(&root_node);
+        let (optimized_node, dur) = time_keisoku(|| optimize(&root_node));
+        root_node = optimized_node;
+
+        info!("optimize: {}ms", dur.as_millis());
     }
 
-    let step_count = InterPrinter::builder()
-        .root_node(&root_node)
-        .input(io::stdin())
-        .output(io::stdout())
-        .build()
-        .count();
+    let (step_count, dur) = time_keisoku(|| {
+        InterPrinter::builder()
+            .root_node(&root_node)
+            .input(io::stdin())
+            .output(io::stdout())
+            .build()
+            .count()
+    });
 
+    info!("run: {}ms", dur.as_millis());
     eprintln!("step: {step_count}");
 }
