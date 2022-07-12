253f1a8fc0fe257745f84bd1316eadd1d992d3da
hotate29 <hotate_oc@yahoo.co.jp>
2022-07-13 00:37:35 +0900

    Varを書き換え

--- bf/src/transcompile/wasm/wasm_binary/var.rs
+++ bf/src/transcompile/wasm/wasm_binary/var.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::io::{self, Write};
 
 pub trait VarInt {}
 
@@ -10,124 +10,92 @@ impl VarInt for u32 {}
 impl VarInt for i64 {}
 
 pub struct Var<T: VarInt> {
-    value: Vec<u8>,
-    _marker: PhantomData<T>,
+    value: T,
 }
 
 pub trait VarImpl<T: VarInt> {
     #[allow(clippy::new_ret_no_self)]
     fn new(value: T) -> Var<T>;
-    fn read(&self) -> T;
+    fn value(&self) -> T;
+    fn write(&self, writer: impl Write) -> io::Result<()>;
 }
 
 impl VarImpl<bool> for Var<bool> {
     fn new(value: bool) -> Var<bool> {
-        let mut buffer = Vec::new();
-        leb128::write::unsigned(&mut buffer, value as u64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> bool {
-        let value = leb128::read::unsigned(&mut self.value.as_slice()).unwrap();
-
-        // u64からboolへの`(Try)Into`実装が無い
-        if value == 1 {
-            true
-        } else if value == 0 {
-            false
-        } else {
-            unreachable!()
-        }
+    fn value(&self) -> bool {
+        self.value
+    }
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        let value = self.value;
+        leb128::write::unsigned(&mut w, value as u64)?;
+        Ok(())
     }
 }
 
 // 以下繰り返し
 impl VarImpl<u8> for Var<u8> {
     fn new(value: u8) -> Var<u8> {
-        let mut buffer = Vec::new();
-        leb128::write::unsigned(&mut buffer, value as u64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> u8 {
-        leb128::read::unsigned(&mut self.value.as_slice())
-            .unwrap()
-            .try_into()
-            .unwrap()
+    fn value(&self) -> u8 {
+        self.value
+    }
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        leb128::write::unsigned(&mut w, self.value as u64)?;
+        Ok(())
     }
 }
 
 impl VarImpl<i8> for Var<i8> {
     fn new(value: i8) -> Var<i8> {
-        let mut buffer = Vec::new();
-        leb128::write::signed(&mut buffer, value as i64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> i8 {
-        leb128::read::signed(&mut self.value.as_slice())
-            .unwrap()
-            .try_into()
-            .unwrap()
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        leb128::write::signed(&mut w, self.value as i64)?;
+        Ok(())
+    }
+    fn value(&self) -> i8 {
+        self.value
     }
 }
 
 impl VarImpl<u32> for Var<u32> {
     fn new(value: u32) -> Var<u32> {
-        let mut buffer = Vec::new();
-        leb128::write::unsigned(&mut buffer, value as u64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> u32 {
-        leb128::read::unsigned(&mut self.value.as_slice())
-            .unwrap()
-            .try_into()
-            .unwrap()
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        leb128::write::unsigned(&mut w, self.value as u64)?;
+        Ok(())
+    }
+    fn value(&self) -> u32 {
+        self.value
     }
 }
 
 impl VarImpl<i32> for Var<i32> {
     fn new(value: i32) -> Var<i32> {
-        let mut buffer = Vec::new();
-        leb128::write::signed(&mut buffer, value as i64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> i32 {
-        leb128::read::signed(&mut self.value.as_slice())
-            .unwrap()
-            .try_into()
-            .unwrap()
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        leb128::write::signed(&mut w, self.value as i64)?;
+        Ok(())
+    }
+    fn value(&self) -> i32 {
+        self.value
     }
 }
 
 impl VarImpl<i64> for Var<i64> {
     fn new(value: i64) -> Var<i64> {
-        let mut buffer = Vec::new();
-        leb128::write::signed(&mut buffer, value as i64).unwrap();
-        Self {
-            value: buffer,
-            _marker: PhantomData,
-        }
+        Self { value }
     }
-
-    fn read(&self) -> i64 {
-        leb128::read::signed(&mut self.value.as_slice()).unwrap()
+    fn write(&self, mut w: impl Write) -> io::Result<()> {
+        leb128::write::signed(&mut w, self.value)?;
+        Ok(())
+    }
+    fn value(&self) -> i64 {
+        self.value
     }
 }
