462f460a02fa0e660f71d515c92c76333d2876cf
hotate29 <hotate_oc@yahoo.co.jp>
2022-07-13 00:42:50 +0900

    VarをTuple構造体にした

--- bf/src/transcompile/wasm/wasm_binary.rs
+++ bf/src/transcompile/wasm/wasm_binary.rs
@@ -1,12 +1,83 @@
 const WASM_BINARY_MAGIC: u32 = 0x6d736100; // \0asm
-const WASM_VERSION: u32 = 1;
+const WASM_BINARY_VERSION: u32 = 1;
 
 mod var;
 
+use std::io::{self, Write};
+
 use var::{Var, VarImpl};
 
+enum Type {
+    I32,
+    I64,
+    F32,
+    F64,
+    AnyFunc,
+    Func {
+        params: Vec<Type>,
+        result: Option<Box<Type>>,
+    },
+    Void,
+}
+impl Type {
+    fn opcode_int(&self) -> i8 {
+        match self {
+            Type::I32 => -1,
+            Type::I64 => -2,
+            Type::F32 => -3,
+            Type::F64 => -4,
+            Type::AnyFunc => -10,
+            Type::Func { .. } => -20,
+            Type::Void => -40,
+        }
+    }
+    fn opcode_var(&self) -> Var<i8> {
+        Var(self.opcode_int())
+    }
+    fn write(&self, mut writer: impl Write) -> io::Result<()> {
+        match self {
+            Type::I32 | Type::I64 | Type::F32 | Type::F64 | Type::Void => {
+                self.opcode_var().write(writer)
+            }
+            // AnyFuncを使う予定は無いのでunimplemented!
+            Type::AnyFunc => unimplemented!(),
+            Type::Func { params, result } => {
+                self.opcode_var().write(&mut writer)?;
+
+                let params_len = Var(params.len() as u32);
+                params_len.write(&mut writer)?;
+
+                for param_type in params {
+                    param_type.write(&mut writer)?
+                }
+
+                if let Some(result) = result {
+                    Var(true).write(&mut writer)?;
+                    result.write(writer)?
+                } else {
+                    Var(false).write(&mut writer)?;
+                }
+
+                Ok(())
+            }
+        }
+    }
+}
+
+pub struct Module {}
+impl Module {
+    pub fn new() -> Self {
+        Module {}
+    }
+
+    pub fn write(&self, mut writer: impl Write) -> io::Result<()> {
+        writer.write_all(WASM_BINARY_MAGIC.to_le_bytes().as_slice())?;
+        writer.write_all(WASM_BINARY_VERSION.to_le_bytes().as_slice())?;
+        Ok(())
+    }
+}
 enum Section {
-    Type,
+    Type(Type),
     Import,
     Function,
     Table,
--- bf/src/transcompile/wasm/wasm_binary/var.rs
+++ bf/src/transcompile/wasm/wasm_binary/var.rs
@@ -9,26 +9,15 @@ impl VarInt for i32 {}
 impl VarInt for u32 {}
 impl VarInt for i64 {}
 
-pub struct Var<T: VarInt> {
-    value: T,
-}
+pub struct Var<T: VarInt>(pub T);
 
 pub trait VarImpl<T: VarInt> {
-    #[allow(clippy::new_ret_no_self)]
-    fn new(value: T) -> Var<T>;
-    fn value(&self) -> T;
     fn write(&self, writer: impl Write) -> io::Result<()>;
 }
 
 impl VarImpl<bool> for Var<bool> {
-    fn new(value: bool) -> Var<bool> {
-        Self { value }
-    }
-    fn value(&self) -> bool {
-        self.value
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        let value = self.value;
+        let value = self.0;
         leb128::write::unsigned(&mut w, value as u64)?;
         Ok(())
     }
@@ -36,66 +25,36 @@ impl VarImpl<bool> for Var<bool> {
 
 // 以下繰り返し
 impl VarImpl<u8> for Var<u8> {
-    fn new(value: u8) -> Var<u8> {
-        Self { value }
-    }
-    fn value(&self) -> u8 {
-        self.value
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        leb128::write::unsigned(&mut w, self.value as u64)?;
+        leb128::write::unsigned(&mut w, self.0 as u64)?;
         Ok(())
     }
 }
 
 impl VarImpl<i8> for Var<i8> {
-    fn new(value: i8) -> Var<i8> {
-        Self { value }
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        leb128::write::signed(&mut w, self.value as i64)?;
+        leb128::write::signed(&mut w, self.0 as i64)?;
         Ok(())
-    }
-    fn value(&self) -> i8 {
-        self.value
     }
 }
 
 impl VarImpl<u32> for Var<u32> {
-    fn new(value: u32) -> Var<u32> {
-        Self { value }
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        leb128::write::unsigned(&mut w, self.value as u64)?;
+        leb128::write::unsigned(&mut w, self.0 as u64)?;
         Ok(())
-    }
-    fn value(&self) -> u32 {
-        self.value
     }
 }
 
 impl VarImpl<i32> for Var<i32> {
-    fn new(value: i32) -> Var<i32> {
-        Self { value }
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        leb128::write::signed(&mut w, self.value as i64)?;
+        leb128::write::signed(&mut w, self.0 as i64)?;
         Ok(())
-    }
-    fn value(&self) -> i32 {
-        self.value
     }
 }
 
 impl VarImpl<i64> for Var<i64> {
-    fn new(value: i64) -> Var<i64> {
-        Self { value }
-    }
     fn write(&self, mut w: impl Write) -> io::Result<()> {
-        leb128::write::signed(&mut w, self.value)?;
+        leb128::write::signed(&mut w, self.0)?;
         Ok(())
-    }
-    fn value(&self) -> i64 {
-        self.value
     }
 }
