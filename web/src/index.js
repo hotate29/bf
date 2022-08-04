927c7b6f7e4e19512acd37a0c65c74f2bcf1bfc5
hotate29 <hotate_oc@yahoo.co.jp>
2022-08-04 18:50:58 +0900

    abort

--- web/src/index.js
+++ web/src/index.js
@@ -4,11 +4,17 @@ const start_button = document.querySelector('button[id="start"]');
 start_button.addEventListener('click', run);
 
 // まだ
-// const abort_button = document.querySelector('button[id="abort"]');
-// abort_button.addEventListener('click', run);
+const abort_button = document.querySelector('button[id="abort"]');
+abort_button.disabled = true;
+abort_button.addEventListener('click', abort);
+
+const p = document.querySelector('p');
 
 async function run() {
+    start_button.disabled = true;
+
     const worker = new Worker(new URL('./worker.js', import.meta.url));
+    window.worker = worker
 
     const bf_element = document.querySelector('textarea[id="bf"]');
     const bf = bf_element.value;
@@ -32,8 +38,9 @@ async function run() {
     const transpile_time = end_transpile - start_transpile;
 
     worker.postMessage({ module: module, stdin: stdin })
+    abort_button.disabled = false;
 
-    const p = document.querySelector('p');
+    p.textContent = "Running..."
 
     stdout_pre.textContent = ""
 
@@ -46,7 +53,15 @@ async function run() {
         if (typeof msg.exec_time === 'number') {
             const exec_time = msg.exec_time
             p.textContent = `Transpile: ${transpile_time}ms Execution: ${exec_time}ms`;
+            start_button.disabled = false;
+            abort_button.disabled = true;
         }
     }
 }
 
+async function abort() {
+    window.worker.terminate();
+    abort_button.disabled = true;
+    start_button.disabled = false;
+    p.textContent = "Aborted"
+}
