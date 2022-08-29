0c81ca16e8a0066b551796d4f5526257bbf13c21
hotate29 <hotate_oc@yahoo.co.jp>
2022-08-30 00:45:31 +0900

    コメントアウト

--- bf/src/transpile/wasm/wasm_binary/type_.rs
+++ bf/src/transpile/wasm/wasm_binary/type_.rs
@@ -4,10 +4,10 @@ use super::var::Var;
 
 pub enum Type {
     I32,
-    I64,
-    F32,
-    F64,
-    AnyFunc,
+    // I64,
+    // F32,
+    // F64,
+    // AnyFunc,
     Func {
         params: Vec<Type>,
         result: Option<Box<Type>>,
@@ -18,10 +18,10 @@ impl Type {
     fn opcode_int(&self) -> i8 {
         match self {
             Type::I32 => -0x1,
-            Type::I64 => -0x2,
-            Type::F32 => -0x3,
-            Type::F64 => -0x4,
-            Type::AnyFunc => -0x10,
+            // Type::I64 => -0x2,
+            // Type::F32 => -0x3,
+            // Type::F64 => -0x4,
+            // Type::AnyFunc => -0x10,
             Type::Func { .. } => -0x20,
             Type::Void => -0x40,
         }
@@ -33,11 +33,9 @@ impl Type {
     // https://doc.rust-lang.org/error-index.html#E0275
     pub fn write(&self, mut w: &mut dyn Write) -> io::Result<()> {
         match self {
-            Type::I32 | Type::I64 | Type::F32 | Type::F64 | Type::Void => {
-                self.opcode_var().write(w)
-            }
+            Type::I32 | Type::Void => self.opcode_var().write(w),
             // AnyFuncを使う予定は無いのでunimplemented!
-            Type::AnyFunc => unimplemented!(),
+            // Type::AnyFunc => unimplemented!(),
             Type::Func { params, result } => {
                 // Func
                 self.opcode_var().write(&mut w)?;
